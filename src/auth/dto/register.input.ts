964acf1f2de12647d2184a60604bc6971b805ef4
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-09-10 06:28:56 +0600

    :x: Removing everything old

--- src/auth/dto/register.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType } from '@nestjs/graphql';
-
-@InputType({ description: 'Inputs to register a new author' })
-export class RegisterInput {
-  name: string;
-  walletAddress: string;
-}
--- src/auth/entities/auth.entity.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from '@nestjs/graphql';
-
-@ObjectType({ description: 'Author Model' })
-export class Auth {
-  access_token: string;
-}
