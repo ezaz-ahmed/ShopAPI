bc8962a9b125f609b5601cfe0513d8776134a6f4
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-09-10 13:04:01 +0600

    :pencil: (model): User model create

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
   port: 1337,
-  dbUri: "mongodb+srv://db-user:Ihm6hbJOI0UwqKJn@craft.mjwg4lu.mongodb.net/?retryWrites=true&w=majority"
+  dbUri: "mongodb+srv://db-user:Ihm6hbJOI0UwqKJn@craft.mjwg4lu.mongodb.net/?retryWrites=true&w=majority",
+  saltWorkFactor: 10
 }
---
+++ src/models/user.model.ts
@@ -0,0 +1,55 @@
+import { Schema, model, Document } from "mongoose"
+import bcrypt from "bcrypt"
+import config from 'config'
+
+interface IUser extends Document {
+  name: string
+  email: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
+  comparePassword(candidatePassword: string): Promise<boolean>
+}
+
+const userSchema: Schema = new Schema<IUser>({
+  email: {
+    type: String,
+    requried: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  name: {
+    type: String,
+    requried: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    requried: true,
+  }
+}, {
+  timestamps: true
+})
+
+userSchema.pre<IUser>("save", async function (next: (err?: Error) => void) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"))
+  const hash = bcrypt.hashSync(this.password, salt)
+  this.password = hash
+
+  return next()
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  return bcrypt
+    .compare(candidatePassword, this.password)
+    .catch(e => false)
+}
+
+const UserModel = model<IUser>("User", userSchema)
+
+export default UserModel
