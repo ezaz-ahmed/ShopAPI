8cf80d761a625667529697a22b6e280fa0afe9a5
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-09-11 16:27:21 +0600

    :rocket: (resource): Get All Session hnalder complete

---
+++ src/middleware/requireUser.ts
@@ -0,0 +1,21 @@
+import { verify } from "crypto"
+import { Request, Response, NextFunction } from "express"
+import { get } from "lodash"
+import { verifyJwt } from "../utils/jwt.utils"
+
+const requireUser = (req: Request, res: Response, next: NextFunction) => {
+  const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "")
+
+  if (!accessToken) {
+    return next()
+  }
+
+  const { decoded, expired } = verifyJwt(accessToken)
+
+  if (decoded) {
+    res.locals.user = decoded
+    return next()
+  }
+}
+
+export default requireUser
